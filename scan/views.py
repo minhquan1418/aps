b6a4cc4f909a4eaa210fd03dc9ec18f9befeb78d
minhquan1418 <lephamminhquan1418@gmail.com>
2019-05-08 00:03:58 +0700

    test cac tool

--- scan/views.py
+++ scan/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
+import subprocess
 
 def index(request):
-    data = {'key': 'value'}
+    # lay tat ca cac file apk
+    # quy cach: se khong co duoi .apk
+    # quy cach: 1 file apk tuong ung voi 1 folder trong temp/
+    list_apk = ['UnCrackable-Level1', 'UnCrackable-Level2', 'UnCrackable-Level2']
+    
+    # chuyen apk -> jar (dung jadx)
+    # for apk in list_apk:
+        
+
+    # qark
+    data = {'key': subprocess.check_output(["qark", "--apk", "apk_test/UnCrackable-Level1.apk", "--report-type", "json"])}
+    # output /usr/local/lib/python2.7/dist-packages/qark/report/report.json
     return JsonResponse(data)
