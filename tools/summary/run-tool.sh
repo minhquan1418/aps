13dc0f7ee03b89b2b8c9f6303cb029c9c1320fcf
minhquan1418 <lephamminhquan1418@gmail.com>
2019-05-15 22:25:49 +0700

    run tool

--- tools/summary/run-tool.sh
+++ tools/summary/run-tool.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-#=/home/vladimir/Downloads/genymotion/tools/
-#dex2jar=/home/vladimir/thesis/aps/aps-1/tools/dex2jar/d2j-dex2jar.sh
-#dir_file=/home/vladimir/thesis/aps/aps-1/outdir/file
-#add_procyon=/home/vladimir/thesis/aps/aps-1/tools/procyon-decompiler.jar
-#dir_source=/home/vladimir/thesis/aps/aps-1/outdir/source
-#owasp_check=/home/vladimir/thesis/aps/aps-1/tools/dependency-check/bin/dependency-check.sh
-#dir_report=/home/vladimir/thesis/aps/aps-1/outdir/report
 
 dex2jar=tools/dex2jar/d2j-dex2jar.sh
 dir_file=outdir/file
@@ -16,9 +9,9 @@ dir_report=outdir/report
 
 # The command pull file apk so that analysis
 mkdir $dir_file/$1
-echo "nhay vao"
-/home/vladimir/Downloads/genymotion/tools/adb pull /data/app/"$1"/base.apk $dir_file/$1
-echo "thoat"
+
+tools/adb pull /data/app/"$1"/base.apk $dir_file/$1
+
 return 0
 # Command excute 2 tool dex2jar and owasp_check
 sh $dex2jar -f $dir_file/$1/base.apk
