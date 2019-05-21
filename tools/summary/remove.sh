0fce9f99201863228b2ed98730fa1eb537830db8
minhquan1418 <lephamminhquan1418@gmail.com>
2019-05-21 22:04:40 +0700

    qark

---
+++ tools/summary/remove.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf outdir/file/*
+rm -rf outdir/source/*
+rm -rf outdir/report/*
--- tools/summary/run-tool.sh
+++ tools/summary/run-tool.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
-
-dex2jar=../tools/dex2jar/d2j-dex2jar.sh
 dir_file=outdir/file
-add_procyon=../tools/procyon-decompiler.jar
 dir_source=outdir/source
-owasp_check=../tools/dependency-check/bin/dependency-check.sh
 dir_report=outdir/report
+owasp_check=../tools/dependency-check/bin/dependency-check.sh
 
 # The command pull file apk so that analysis
 mkdir $dir_file/$1
 
-../tools/adb pull /data/app/"$1"/base.apk $dir_file/$1
+# pull file apk
+../tools/adb pull /data/app/$1 $dir_file/$1
+#../tools/adb pull /data/app/"$1"/base.apk $dir_file/$1
+#cp $dir_file/$1/base.apk $dir_file/$1/$1
 
-return 0
-# Command excute 2 tool dex2jar and owasp_check
-sh $dex2jar -f $dir_file/$1/base.apk
-java -jar $add_procyon base-dex2jar.jar -o $dir_source/$1
-$owasp_check --project Check_Security --out $dir_report/$1 --scan base-dex2jar.jar -n -f JSON
+# qark
+mkdir $dir_source/$1
+qark --apk $dir_file/$1/$1 --build-path $dir_source/$1 --report-type json
+
+# dependency-check
+pn=$(echo "$1" | sed -e "s/.apk$//")
+$owasp_check --project Check_Security --out $dir_report/$1 --scan $dir_source/$1/qark/"$pn.jar" -n -f JSON 
 
 # using sniffgit so that find sensitive infomation in source code
-sniffgit --root $dir_source/$1 > $dir_report/$1/sniffgit_report.txt
-cat $dir_report/$1/sniffgit_report.txt
+mkdir $dir_source/$1
+sniffgit --root $dir_source/$1/qark/procyon > $dir_report/$1/sniffgit_report.txt
 #grep -w 'username\|password\|email\|key' $dir_report/$1/sniffgit_report.txt > $dir_report/sniffgit_report.txt
 #grep -w 'username\|password\|email\|key\|token' $dir_report/$1/sniffgit_report.txt > $dir_report/$1/sniffgit_report.txt
