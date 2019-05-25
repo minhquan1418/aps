c5bdc7ba23d71af85d18947723c95a9d38c07d9a
minhquan1418 <lephamminhquan1418@gmail.com>
2019-05-25 21:23:06 +0700

    add andro

--- tools/summary/run-tool.sh
+++ tools/summary/run-tool.sh
@@ -23,5 +23,11 @@ $owasp_check --project Check_Security --out $dir_report/$1 --scan $dir_source/$1
 # using sniffgit so that find sensitive infomation in source code
 mkdir $dir_source/$1
 sniffgit --root $dir_source/$1/qark/procyon > $dir_report/$1/sniffgit_report.txt
-#grep -w 'username\|password\|email\|key' $dir_report/$1/sniffgit_report.txt > $dir_report/sniffgit_report.txt
-#grep -w 'username\|password\|email\|key\|token' $dir_report/$1/sniffgit_report.txt > $dir_report/$1/sniffgit_report.txt
+
+# andro bug framework
+python ../tools/andro_bug/androbugs.py -f $dir_file/$1/base.apk -o $dir_report/$1/
+
+# manitree
+cd ../tools/manitree/ # if location is "aps", manitree will not find adb and throw erro
+python manitree.py -f ../../aps/$dir_file/$1/base.apk >> ../../aps/$dir_report/$1/manitree_app.txt
+cd ../../aps/
